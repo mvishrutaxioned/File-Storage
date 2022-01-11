492504a10df5587463980b83f42cc8712b7d44f2
VishrutMargam <vishrutm@axioned.com>
2022-01-11 18:25:44 +0530

    added displayData functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,43 @@ $(document).ready(() => {
         }
     ];
 
+    // call displayData function
+    displayData()
+
+    // displayData function
+    function displayData() {
+        $('tbody').html('')
+        content = '';
+
+        if(data.length) {
+            for (var i of data) {
+                content += `
+                <tr>
+                    <td>${i.name}</td>
+                    <td>${i.value}</td>
+                    <td><a href="#FIXME" title="Modify" class="modify" data-id="${i.id}">Edit</a></td>
+                    <td><a href="#FIXME" title="Remove" class="remove" data-id="${i.id}">Del</a></td>
+                </tr>
+                `;
+            }
+            $('tbody').html(content);
+        } else {
+            $('.sorry').show();
+            console.log('sorry')
+        }
+
+        // delete button functionality
+        $('.remove').each(function(i, elem) {
+            $(elem).click((r) => {
+                r.preventDefault;
+
+                var myId = $(elem).data('id')
+                $(data).each(function(index, value) { if(value.id == myId) data.splice(index, 1) });
+                displayData();
+            })
+        })
+    }
+
     $('#addData').submit((e) => {
         e.preventDefault();
 
